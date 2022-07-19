b8bf8c636648e9ef76bfb688d3132398d0b8a80e
Takuya-Endo <above5purple@gmail.com>
2022-07-19 19:57:54 +0900

    [Update]PythonCompleteIntroduction-013

--- PythonCompleteIntroduction/Chapter4/iterable.py
+++ PythonCompleteIntroduction/Chapter4/iterable.py
@@ -2,3 +2,8 @@ list_a = ["A", "B", "C", "D", "E"]
 print(list_a, id(list_a))
 list_a = list("ABCDE")
 print(list_a, id(list_a))
+
+print(list_a[1])
+print(list_a[-2])
+print(list_a[1:4])
+print(list_a[::2])
