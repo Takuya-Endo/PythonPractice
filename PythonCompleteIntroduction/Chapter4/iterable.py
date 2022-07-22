ce77736154153a8803b9aaea255431d708384ec0
Takuya-Endo <above5purple@gmail.com>
2022-07-22 16:58:20 +0900

    [Update]PythonCompleteIntroduction-017

--- PythonCompleteIntroduction/Chapter4/iterable.py
+++ PythonCompleteIntroduction/Chapter4/iterable.py
@@ -39,7 +39,7 @@ list_b = list_b[:3]
 list_b[len(list_b):] = list("DE")
 print(list_b)
 
-del(list_b[4])
+del list_b[4]
 print(list_b)
 list_b.pop(3)
 print(list_b)
@@ -47,3 +47,27 @@ list_b.remove("C")
 print(list_b)
 list_b += list("CDE")
 print(list_b)
+del list_b[2:3]
+print(list_b)
+list_b.insert(2, "C")
+print(list_b)
+list_b[2:3] = []
+print(list_b)
+list_b[1:2] += list("C")
+print(list_b)
+
+string_b = ", ".join(list_b)
+print(string_b)
+list_c = string_b.split(",")
+print(list_c)
+
+print(len(list_c))
+print(list_c*3)
+print(list_c*0)
+
+list_d = list("AECDB")
+print(list_d)
+list_d.sort()
+print(list_d)
+list_d.sort(reverse=True)
+print(list_d)
