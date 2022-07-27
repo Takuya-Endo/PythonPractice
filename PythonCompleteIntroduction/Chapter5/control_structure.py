b0eb4b404b058761f52b8b616bbfd1e343cfc49d
Takuya-Endo <above5purple@gmail.com>
2022-07-28 07:46:12 +0900

    [Update]PythonCompleteIntroduction-026

--- PythonCompleteIntroduction/Chapter5/control_structure.py
+++ PythonCompleteIntroduction/Chapter5/control_structure.py
@@ -7,3 +7,7 @@ elif str == "B":
     print("string is B")
 else:
     print("string is other")
+
+sex = "MAN"
+# sex = "WOMAN"
+print("SEX is MAN") if sex == "MAN" else print("SEX is WOMAN")  # 三項演算子
