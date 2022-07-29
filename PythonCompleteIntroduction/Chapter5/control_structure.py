8b7f622773b3a6009339cb9d62f1a3908e059e3c
Takuya-Endo <above5purple@gmail.com>
2022-07-29 17:49:11 +0900

    [Update]PythonCompleteIntroduction-027

--- PythonCompleteIntroduction/Chapter5/control_structure.py
+++ PythonCompleteIntroduction/Chapter5/control_structure.py
@@ -11,3 +11,10 @@ else:
 sex = "MAN"
 # sex = "WOMAN"
 print("SEX is MAN") if sex == "MAN" else print("SEX is WOMAN")  # 三項演算子
+
+list_a = list("ABCDE")
+for element_a in list_a:
+    print(element_a, end=" ")
+    for str in "abcde":
+        print(str, end=", ")
+    print()
